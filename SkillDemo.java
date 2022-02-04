9f58d65cfd5dfa0b2ad3b03a3b3b26d04b149817
winniecibo <winniechen.cibo@gmail.com>
2022-02-04 10:31:07 -0800

    failed test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,6 @@
 
+public class SkillDemo {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multiply() {
+        assertEquals(2, SkillDemo.multiply);
+    }
+}
